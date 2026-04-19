9683173d5a74203096dacee163bb8f73faf555da
Osama <osama22@gmail.com>
2026-04-19 23:21:12 +0200

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://myway-egypt.vercel.app/';
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.myway-egypt.com';
     return [
         {
             url: siteUrl,
